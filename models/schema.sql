8bd4ef4b5567a98b4abd4506866d1d56a8781544
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2019-02-02 13:15:02 -0800

    schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,32 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS recipes_db;
+CREATE DATABASE recipes_db;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+USE recipes_db;
+
+CREATE TABLE recipes(
+recipe_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+recipe_name VARCHAR(100),
+description VARCHAR(500),
+recipe_image VARCHAR(100),
+prep_time INTEGER,
+rating INTEGER
+);
+
+CREATE TABLE products(
+product_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+product_name VARCHAR(100),
+product_image VARCHAR(100),
+product_calories INTEGER,
+gluten_free BOOLEAN,
+vegetarian BOOLEAN
+);
+
+CREATE TABLE ingredients(
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+recipe_id INTEGER,
+product_id INTEGER,
+amount INTEGER
+)
