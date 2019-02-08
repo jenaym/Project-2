f381bb7afdcfe1fe28f2472f6a8ba5c0ddb4bf7e
maryarzumanyan <37827950+maryarzumanyan@users.noreply.github.com>
2019-02-07 18:30:11 -0800

    Core API routes are in. SQL Seeds are in. (#11)
    * schema.sql
    
    * Sequelize models and associations are created.
    
    * Core API routes are in. SQL Seeds are in.

--- models/ingredients.js
+++ models/ingredients.js
@@ -3,10 +3,9 @@
 
 module.exports = function(sequelize, DataTypes) {
     var Ingredients = sequelize.define("Ingredients", {
-        name: DataTypes.STRING,
+        // name: DataTypes.STRING,
         amount: DataTypes.INTEGER
     });
     
     return Ingredients;
-  };
-  
+};
--- models/products.js
+++ models/products.js
@@ -4,15 +4,12 @@
 
 module.exports = function(sequelize, DataTypes) {
     var Products = sequelize.define("Products", {
-      
-     //   product_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        name: DataTypes.STRING,
-        image: DataTypes.STRING,
-        calories: DataTypes.INTEGER,
+        product_name: DataTypes.STRING,
+        product_image: DataTypes.STRING,
+        product_calories: DataTypes.INTEGER,
         gluten_free: DataTypes.BOOLEAN,
         vegetarian: DataTypes.BOOLEAN
     });
     
     return Products;
-  };
-  
+};
