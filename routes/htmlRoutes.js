2112dc88b6a6d423b80b496984c5eda46669d4c3
Motohiko Makino <motohiko.makino@gmail.com>
2019-02-09 13:03:23 -0800

    Resolved overlooked conflict resolutions. 	modified:   routes/apiRoutes.js 	modified:   routes/htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -32,17 +32,8 @@ module.exports = function(app) {
 		res.render("search");
 	});
 
-<<<<<<< HEAD
 	// Render 404 page for any unmatched routes
 	app.get("*", function(req, res) {
 		res.render("404");
 	});
-=======
-
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
->>>>>>> master
 };
