38da8878bb76233799909619ccbda2c11e9017be
jenaym <43564263+jenaym@users.noreply.github.com>
2019-02-13 21:41:48 -0800

    fully functioning post page (#22)

--- models/index.js
+++ models/index.js
@@ -1,67 +1,3 @@
-// "use strict";
-
-// var fs = require("fs");
-// var path = require("path");
-// var Sequelize = require("sequelize");
-// var basename = path.basename(module.filename);
-// var env = process.env.NODE_ENV || "development";
-// var config = require(__dirname + "/../config/config.json")[env];
-// var db = {};
-
-// require('dotenv').config();
-
-// // * * * * * * * * *
-// // CREATE "recipes_db" IF NOT EXISTS
-// const mysql = require('mysql2');
-// const connection = mysql.createConnection({
-//   host: config.host,
-//   // port: 3306,
-//   user: config.username,
-//   password: process.env.PASSWORD,
-// });
-// connection.execute(`CREATE DATABASE IF NOT EXISTS ${config.database}`);
-// // * * * * * * * * *
-
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password = process.env.PASSWORD,
-//     config
-//   );
-// }
-
-// // Load models into sequialize
-// fs.readdirSync(__dirname)
-//   .filter(function (file) {
-//     return (
-//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-//     );
-//   })
-//   .forEach(function (file) {
-//     var model = sequelize.import(path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
-
-// // TODO: Model does not have associate prop or method. What is this then?
-// Object.keys(db).forEach(function (modelName) {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// // Associations
-// db["Recipes"].hasMany(db["RecipeIngredient"]);
-// db["Ingredient"].hasMany(db["RecipeIngredient"]);
-// db["Measure"].hasMany(db["RecipeIngredient"]);
-
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize; // TODO: Why do we need it?
-
-// module.exports = db;
 "use strict";
 
 var fs = require("fs");
@@ -120,6 +56,7 @@ Object.keys(db).forEach(function (modelName) {
 // build associations
 db["Products"].belongsToMany(db["Recipes"], { through: db["Ingredients"] });
 db["Recipes"].belongsToMany(db["Products"], { through: db["Ingredients"] });
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
--- public/js/post.js
+++ public/js/post.js
@@ -1,33 +1,66 @@
-$(document).ready(function() {
-	var maxIngredients = 100;
-	var x = 1; //Initial ingredient counter
-	var ingredientList = $(".allIngredients");
-	var newName = "<div class=\"form-group col-md-3\"><label>Ingredients</label><input id=\"ingredient\" type=\"text\" class=\"form-control\" placeholder=\"Ingredient\"></div>";
-	var newQuantity = "<div class=\"form-group col-md-3\"><label>Quantity</label><input id=\"quantity\" type=\"text\" class=\"form-control\" placeholder=\"Whole #\"></div>";
-	var newUnit = "<div class=\"form-group col-md-3\">" +
-        "<label>Unit</label>" + 
-            "<div>" +
-            "<select id=\"unit\">" +
-                "<option selected>Choose...</option>" +
-                "<option value=\"teaspoon\">teaspoon</option>" +
-                "<option value=\"tablespoon\">tablespoon</option>" +
-                "<option value=\"cup\">cup</option>" +
-                "<option value=\"ounce\">ounce</option>" +
-                "<option value=\"pint\">pint</option>" +
-                "<option value=\"quart\">quart</option>" +
-                "<option value=\"pound\">pound</option>" +
-            "</select>" +
-            "</div>" +
-        "</div>";
+$(document).ready(function () {
+    var maxIngredients = 100;
+    var x = 1; //Initial ingredient counter
+    var ingredientList = $(".allIngredients");
 
-	var newIngredient = ("<div class=\"form-row\">" + newName + newQuantity + newUnit + "</div>");
+    $(".addIngredient").on("click", function (event) {
+        event.preventDefault();
+        console.log(`clicked ${x}`);
+        if (x < maxIngredients) {
+            x++;
 
-	$(".addIngredient").on("click", function(event) {
-		event.preventDefault();
-		if (x < maxIngredients) {
-			x++;
-			$(ingredientList).append(newIngredient);
-		}
-	});
+            ingredientList.append(makeNewIngredient(x));
+        }
+    });
 });
-		
+
+//
+// Construct html elements for a new ingredient 
+//
+function makeNewIngredient(counter) {
+    let newIngredient = $("<div>");
+
+    let newName = $("<div>");
+    newName.addClass("form-group").addClass("col-md-6");
+    newName.append("<label>Ingredients</label>");
+
+    let newNameInput = $("<input>");
+    newNameInput.attr("type", "text").attr("placeholder", "Ingredient");
+    newNameInput.addClass("form-control");
+
+    let newQty = $("<div>");
+    newQty.addClass("form-group").addClass("col-md-3");
+    newQty.append("<label>Quantity</label>");
+
+    let newQtyInput = $("<input>");
+    newQtyInput.attr("type", "text").attr("placeholder", "Whole #");
+    newQtyInput.addClass("form-control");
+
+    let newUnit = $("<div>");
+    newUnit.addClass("form-group").addClass("col-md-3");
+    newUnit.append("<label>Unit</label>");
+
+    let newUnitInput = $('<select>');
+    newUnitInput.addClass("unit");
+    newUnitInput.append('<option selected>Choose...</option>');
+    newUnitInput.append('<option value="teaspoon">teaspoon</option>');
+    newUnitInput.append('<option value="tablespoon">tablespoon</option>');
+    newUnitInput.append('<option value="cup">cup</option>');
+    newUnitInput.append('<option value="ounce">ounce</option>');
+    newUnitInput.append('<option value="pint">pint</option>');
+    newUnitInput.append('<option value="quart">quart</option>');
+    newUnitInput.append('<option value="pound">pound</option>');
+
+    newIngredient.addClass("form-row").addClass("ingrItem");
+
+    newNameInput.attr("id", `ingrID-${counter}`);
+    newQtyInput.attr("id", `qtyID-${counter}`);
+    newUnitInput.attr("id", `unitID-${counter}`);
+    newUnitInput.wrapAll('<div>');
+
+    newIngredient.append(newName.append(newNameInput));
+    newIngredient.append(newQty.append(newQtyInput));
+    newIngredient.append(newUnit.append(newUnitInput));
+
+    return newIngredient;
+}
